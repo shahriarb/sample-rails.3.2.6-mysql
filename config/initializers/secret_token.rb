271cf5f3db126c90d1bb6de035f7e9a2e4e12500
Luvic van Gool <vic@thecloudblocks.com>
2012-09-06 20:00:46 +0100

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleRails326Mysql::Application.config.secret_token = '9171e126659408f29da652f0c5959739b23d6861e69ed45fe0d33c80333616e89e5d196493189e6d35dfd6c7bb061575a1b4ab20f067af63143f6d0a2cef6612'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+SampleRails326Mysql::Application.routes.draw do
+
+   root :to => 'hill#visit'
+
+end
