9bab3eff76e00978fda926219d28fecab961b78f
ShaB <shab@ubuntu.(none)>
2013-03-22 19:46:44 +0200

    after database migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906184650) do
+ActiveRecord::Schema.define(:version => 20130322072950) do
 
   create_table "buckets", :force => true do |t|
     t.integer  "size"
     t.integer  "weight"
     t.boolean  "is_full"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "contacts", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
